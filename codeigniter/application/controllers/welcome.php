57ff2f83aaa5a935595f94b018aece1b343248bd
Yuta Niwa <kuroko.yn@gmail.com>
2014-05-06 23:06:03 +0900

    ファイル名の変更

--- codeigniter/application/controllers/welcome.php
+++ codeigniter/application/controllers/welcome.php
@@ -17,14 +17,20 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
+
+	//　ログイン画面view
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('login');
 	}
+
+	// 会員登録画面view
 	public function regist()
 	{		
 		$this->load->view('regist');
 	}
+
+	// ツイート画面view
 	public function contribute()
 	{
 		$this->load->view('contribute');
---
+++ codeigniter/application/views/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>ログイン</title>
+</head>
+<body>
+	<div id="container">
+		<form method="post" action="example.php">
+			<p>メールアドレス<input type="text" name="mail"></p>
+			<p>パスワード<input type="password" name="pass"></p>
+			<input type="submit" value="ログイン">
+		</form>
+		<a href="http://vagrant-codeigniter.local/index.php/welcome/regist">ユーザ登録情報はこちら</a>
+	</div>
+</body>
+</html>
