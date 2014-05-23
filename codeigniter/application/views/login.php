4b15eef5718278988ed9ae3f274584fbfb6893c4
Yuta Niwa <kuroko.yn@gmail.com>
2014-05-23 17:48:52 +0900

    login_page css decorate

--- codeigniter/application/views/login.php
+++ codeigniter/application/views/login.php
@@ -2,21 +2,35 @@
 <html lang="en">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <link rel="stylesheet" href="<?=base_url();?>css/bootstrap.min.css"　type="text/css">
+  <link rel="stylesheet" href="<?=base_url();?>css/login.css"　type="text/css">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+  <script src="<?=base_url();?>js/bootstrap.min.js"></script>
+
   <title>ログイン</title>
 </head>
 <body>
   <div id="container">
     <?php echo validation_errors(); ?>
-    <?php echo form_open('login/index') ?>
-      <label>メールアドレス</label>
-      <?php echo form_input('adress',set_value('adress')); ?>
+    <?php echo form_open('login/index'); ?>
+      <label class="adress-label">メールアドレス</label>
+      <?php echo form_input(array(
+          'name' => 'adress',
+          'value' => set_value('adress'),
+          'class' => 'adress-input')); ?>
       <br />
-      <label>パスワード</label>
-      <?php echo form_password('password'); ?>
+      <label class="pass-label">パスワード</label>
+      <?php echo form_password(array(
+          'name' => 'password',
+          'class' => 'pass-input')); ?>
       <br />
-      <?php echo form_submit('','ログイン'); ?>
+      <?php echo form_submit(array(
+          'value' => 'ログイン',
+          'class' => 'login-btn btn btn-warning btn-lg')); ?>
     </form>
-    <a href="http://vagrant-codeigniter.local/index.php/regist/index">ユーザ登録情報はこちら</a>
+    <div class="regist">
+      <a href="http://vagrant-codeigniter.local/index.php/regist/index">ユーザ登録はこちらから</a>
+    </div>
   </div>
 </body>
 </html>
